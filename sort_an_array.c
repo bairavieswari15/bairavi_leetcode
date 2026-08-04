5d3887e5994182a3d534ed5639e0401acaa422db
R Uthayabairavi <bairavieswari15_bai28@mepcoeng.ac.in>
2026-08-04 10:07:45 +0530

    Add files via upload

---
+++ sort_an_array.c
@@ -0,0 +1,43 @@
+void heapify(int* nums, int n, int i)
+ {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && nums[left] > nums[largest])
+     {
+        largest = left;
+    }
+
+    if (right < n && nums[right] > nums[largest])
+     {
+        largest = right;
+    }
+
+    if (largest != i) {
+        int temp = nums[i];
+        nums[i] = nums[largest];
+        nums[largest] = temp;
+        heapify(nums, n, largest);
+    }
+}
+
+int* sortArray(int* nums, int numsSize, int* returnSize)
+ {
+    *returnSize = numsSize;
+
+    for (int i = numsSize / 2 - 1; i >= 0; i--)
+     {
+        heapify(nums, numsSize, i);
+    }
+
+    for (int i = numsSize - 1; i > 0; i--)
+     {
+        int temp = nums[0];
+        nums[0] = nums[i];
+        nums[i] = temp;
+        heapify(nums, i, 0);
+    }
+
+    return nums;
+}
